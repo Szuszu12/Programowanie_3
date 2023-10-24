7524ba70294d05f91650cce820270a370b227dd8
Szuszu12 <szymonolszok@gmail.com>
2023-10-24 21:52:11 +0200

    Dodaj pliki projektów.

---
+++ zad1_Szymon_Olszok/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad1_Szymon_Olszok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string currentInput = "";
+        private string currentOperator = "";
+        private double firstNumber = 0;
+        private bool isNewCalculation = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            if (isNewCalculation)
+            {
+                inputTextBox.Text = "";
+                isNewCalculation = false;
+            }
+
+            Button button = (Button)sender;
+            currentInput += button.Content.ToString();
+            inputTextBox.Text += button.Content.ToString();
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isNewCalculation)
+            {
+                if (!string.IsNullOrEmpty(currentInput))
+                {
+                    firstNumber = double.Parse(currentInput);
+                    currentOperator = ((Button)sender).Content.ToString();
+                    currentInput = "";
+                    inputTextBox.Text += currentOperator;
+                }
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput) && !string.IsNullOrEmpty(currentOperator))
+            {
+                double secondNumber = double.Parse(currentInput);
+                double result = 0;
+
+                switch (currentOperator)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0)
+                            result = firstNumber / secondNumber;
+                        else
+                            inputTextBox.Text = "Error";
+                        break;
+                }
+
+                inputTextBox.Text = result.ToString();
+                currentInput = result.ToString();
+                isNewCalculation = true;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            currentInput = "";
+            currentOperator = "";
+            firstNumber = 0;
+            inputTextBox.Text = "";
+        }
+    }
+}
---
+++ zad2_Szymon_Olszok/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad2_Szymon_Olszok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private char currentPlayer = 'X';
+        private Button[,] buttons = new Button[3, 3];
+        private bool gameEnded = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Button button = new Button
+                    {
+                        Name = $"btn{row}{col}",
+                        Content = string.Empty,
+                        FontSize = 36
+                    };
+                    button.Click += OnButtonClick;
+                    buttons[row, col] = button;
+                    Grid.SetRow(button, row);
+                    Grid.SetColumn(button, col);
+                    grid.Children.Add(button);
+                }
+            }
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (gameEnded)
+            {
+                MessageBox.Show("Gra zakończona. Rozpocznij nową grę.");
+                return;
+            }
+
+            Button button = (Button)sender;
+            if (string.IsNullOrEmpty(button.Content.ToString()))
+            {
+                button.Content = currentPlayer;
+
+                if (CheckForWinner(currentPlayer))
+                {
+                    MessageBox.Show($"Gracz {currentPlayer} wygrywa!");
+                    gameEnded = true;
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("Remis!");
+                    gameEnded = true;
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+        private bool CheckForWinner(char player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i, 0].Content.ToString() == player.ToString() &&
+                    buttons[i, 1].Content.ToString() == player.ToString() &&
+                    buttons[i, 2].Content.ToString() == player.ToString())
+                {
+                    return true;
+                }
+
+                if (buttons[0, i].Content.ToString() == player.ToString() &&
+                    buttons[1, i].Content.ToString() == player.ToString() &&
+                    buttons[2, i].Content.ToString() == player.ToString())
+                {
+                    return true;
+                }
+            }
+
+            if (buttons[0, 0].Content.ToString() == player.ToString() &&
+                buttons[1, 1].Content.ToString() == player.ToString() &&
+                buttons[2, 2].Content.ToString() == player.ToString())
+            {
+                return true;
+            }
+
+            if (buttons[0, 2].Content.ToString() == player.ToString() &&
+                buttons[1, 1].Content.ToString() == player.ToString() &&
+                buttons[2, 0].Content.ToString() == player.ToString())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsBoardFull()
+        {
+            foreach (Button button in buttons)
+            {
+                if (string.IsNullOrEmpty(button.Content.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            foreach (Button button in buttons)
+            {
+                button.Content = string.Empty;
+            }
+            currentPlayer = 'X';
+            gameEnded = false;
+        }
+    }
+}
